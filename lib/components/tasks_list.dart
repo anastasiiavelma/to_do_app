db7acf40629760cb5c1ed119d612965b87b47a94
anastasiiavelma <anastasiia.velma@nure.ua>
2023-05-24 19:14:37 +0300

    added delete task

--- lib/components/tasks_list.dart
+++ lib/components/tasks_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:to_do_app/api/task_model.dart';
 import 'package:intl/intl.dart';
+import 'package:to_do_app/pages/task_detail_screen.dart';
 
 class TaskListScreen extends StatefulWidget {
   const TaskListScreen({super.key});
@@ -14,11 +15,178 @@ class TaskListScreen extends StatefulWidget {
 
 class _TaskListScreenState extends State<TaskListScreen> {
   List<TaskModel> tasks = [];
-  bool? check1 = false;
+
   @override
   void initState() {
     super.initState();
     fetchTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: refreshTasks,
+      child: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+
+          return GestureDetector(
+            onTap: () {
+              openTaskEditScreen(task);
+            },
+            child: Dismissible(
+              key: Key(task.taskId),
+              direction: DismissDirection.endToStart,
+              background: Container(
+                color: Colors.red,
+                child: const Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              onDismissed: (direction) {
+                deleteTask(task.taskId);
+              },
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: task.urgent == 0
+                        ? const Color(0xFFDBDBDB)
+                        : const Color(0xFFFF8989),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      task.type == 1
+                          ? const Icon(
+                              Icons.home_filled,
+                              color: Color(0xFF383838),
+                              size: 16,
+                            )
+                          : const Icon(
+                              Icons.work_outline_outlined,
+                              color: Color(0xFF383838),
+                              size: 16,
+                            ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 11.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                task.description,
+                                style: const TextStyle(
+                                  color: Color(0xFF383838),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                DateFormat('dd.MM.yyyy').format(
+                                    DateTime.parse(task.finishDate.toString())),
+                                style: const TextStyle(
+                                  color: Color(0xFF383838),
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          setState(() {
+                            if (task.status == 2) {
+                              task.status = 1;
+                            } else {
+                              task.status = 2;
+                            }
+                          });
+                          updateTaskStatus(task.taskId, task.status);
+                        },
+                        child: Container(
+                          width: 33,
+                          height: 33,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 1, color: const Color(0xFF383838)),
+                            borderRadius: BorderRadius.circular(10),
+                            color: task.status == 1
+                                ? const Color(0xFFFBEFB4)
+                                : Colors.transparent,
+                          ),
+                          child: task.status == 1
+                              ? const Icon(Icons.check,
+                                  color: Color(0xFF383838), size: 30)
+                              : null,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void openTaskEditScreen(TaskModel task) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const TaskDetailScreen(),
+      ),
+    ).then((updatedTask) {
+      if (updatedTask != null) {
+        updateTaskStatus(updatedTask.taskId, updatedTask.status);
+      }
+    });
+  }
+
+  Future<void> refreshTasks() async {
+    await fetchTasks();
+  }
+
+  Future<void> updateTaskStatus(String taskId, int newStatus) async {
+    final requestBody = {'status': newStatus};
+
+    try {
+      final response = await http.put(
+        Uri.parse('https://to-do.softwars.com.ua//tasks/$taskId'),
+        body: json.encode(requestBody),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        print('Task status updated');
+      } else {
+        print('Failed to update task status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error occurred while updating task status: $e');
+    }
   }
 
   Future<void> fetchTasks() async {
@@ -49,133 +217,20 @@ class _TaskListScreenState extends State<TaskListScreen> {
     }
   }
 
-  Future<void> updateTaskStatus(String taskId, int newStatus) async {
-    final requestBody = {'status': newStatus};
-
+  Future<void> deleteTask(String taskId) async {
     try {
-      final response = await http.put(
-        Uri.parse('https://to-do.softwars.com.ua//tasks/$taskId'),
-        body: json.encode(requestBody),
-        headers: {'Content-Type': 'application/json'},
+      final response = await http.delete(
+        Uri.parse('https://to-do.softwars.com.ua/tasks/$taskId'),
       );
 
       if (response.statusCode == 200) {
-        print('Task status updated');
+        print('Task deleted');
+        await fetchTasks();
       } else {
-        print('Failed to update task status: ${response.statusCode}');
+        print('Failed to delete task: ${response.statusCode}');
       }
     } catch (e) {
-      print('Error occurred while updating task status: $e');
+      print('Error occurred while deleting task: $e');
     }
-  }
-
-  Future<void> refreshTasks() async {
-    await fetchTasks();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: refreshTasks,
-      child: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          final task = tasks[index];
-
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 20,
-              ),
-              decoration: BoxDecoration(
-                color: task.urgent == 0
-                    ? const Color(0xFFDBDBDB)
-                    : const Color(0xFFFF8989),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  task.type == 1
-                      ? const Icon(
-                          Icons.home_filled,
-                          color: Color(0xFF383838),
-                          size: 16,
-                        )
-                      : const Icon(
-                          Icons.work_outline_outlined,
-                          color: Color(0xFF383838),
-                          size: 16,
-                        ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 11.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            task.description,
-                            style: const TextStyle(
-                              color: Color(0xFF383838),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            DateFormat('dd.MM.yyyy').format(
-                                DateTime.parse(task.finishDate.toString())),
-                            style: const TextStyle(
-                              color: Color(0xFF383838),
-                              fontSize: 11,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    behavior: HitTestBehavior.translucent,
-                    onTap: () {
-                      setState(() {
-                        if (task.status == 2) {
-                          task.status = 1;
-                        } else {
-                          task.status = 2;
-                        }
-                      });
-
-                      updateTaskStatus(task.taskId, task.status);
-                    },
-                    child: Container(
-                      width: 33,
-                      height: 33,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            width: 1, color: const Color(0xFF383838)),
-                        borderRadius: BorderRadius.circular(10),
-                        color: task.status == 1
-                            ? const Color(0xFFFBEFB4)
-                            : Colors.transparent,
-                      ),
-                      child: task.status == 1
-                          ? const Icon(Icons.check,
-                              color: Color(0xFF383838), size: 30)
-                          : null,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
   }
 }
